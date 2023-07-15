57fb99c8d504969f1f8119deab670cc8f248da29
Serhii Shabala <sergei181989@gmail.com>
2023-07-15 13:12:07 +0300

    :package: update compiled files and packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       pages: '/src/pages',
       router: '/src/router',
+      assets: '/src/assets',
       components: '/src/components',
       styles: path.resolve(__dirname, './src/styles/styles.scss'),
     },
